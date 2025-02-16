a071d4b372cb4c8177501a474dc25186d056f68a
Nuhcan ATAR <nuhcanatar20@gmail.com>
2025-02-16 19:16:11 +0300

    The Help and Support screen is complete.

--- lib/feature/account/view/application_settings/application_settings_viewmodel.dart
+++
@@ -1,5 +0,0 @@
-import 'package:salonmate/feature/account/view/application_settings/application_settings_view.dart';
-import 'package:salonmate/product/core/base/base_state/base_state.dart';
-
-abstract class ApplicationSettingsViewModel
-    extends BaseState<ApplicationSettingsView> {}
